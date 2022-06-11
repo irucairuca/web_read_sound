b1fc3381e7b3839eaa9c03cebc0c97a750904026
Arai Tatsuhiro <62388961+irucairuca@users.noreply.github.com>
2022-06-11 17:10:51 +0900

    Add files via upload

---
+++ web_read.py
@@ -0,0 +1,24 @@
+from gtts import gTTS
+
+
+import urllib.request as req
+
+from bs4 import BeautifulSoup
+
+
+url =str(input("音声化したいサイトのURLを入力してください"))
+res = req.urlopen(url)
+
+
+
+html = res
+soup = BeautifulSoup(html, 'html.parser')
+#print("タイトル:"+ soup.title.text)
+#print("本文:"+ soup.find(id="main").text)
+
+
+mytext = "タイトル:"+ soup.title.text + "本文:"+ soup.find(id="main").text
+tts = gTTS(text=mytext, lang='ja')
+tts.save(soup.title.text+'.mp3')
+
+print("webからテキストデータを取得して音声データを作成しました")
